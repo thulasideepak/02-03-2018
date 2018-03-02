4bff7c685e8cc21743dd9e69598f1386fe9af775
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-03-02 15:39:30 +0530

    Create CUBOID.C
    calculate the total surface and volume of cuboid

---
+++ CUBOID.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+        int l,w,h,total;
+        printf("\n enter the length :");
+        scanf("%d",&l);
+        printf("\n enter the width :");
+        scanf("%d",&w);
+        printf("\n enter the height :");
+        scanf("%d",&h);
+        total=l*w*h;
+        printf("\n total surface and volume of cuboid:%d",total);
+        return 0;
+        
+}
+
