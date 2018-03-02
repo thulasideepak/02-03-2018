b6ab93300188dbe5cf78d35bad258eb4f34e37b7
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-03-02 16:20:22 +0530

    Update COMPOSITE OR NOT.C
    COMPOSITE OR NOT

--- COMPOSITE OR NOT.C
+++ COMPOSITE OR NOT.C
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-int num,i,flag=0;
-scanf("%d",&num);
-for(i=2;i<=num/2;i++)
+int N,i,flag=0;
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
 {
-if(num%i==0)
+if(n%i==0)
 {
 flag=1;
 break;
